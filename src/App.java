97c974310b8cccd9372f70e78c1b75648ba8a465
ViniciusTerra <viniterra33@gmail.com>
2023-01-09 21:07:43 -0300

    second commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) {
         
         int x = 2;
-        int[] numeros = {1,2,3,4,5,6,7,8,9};
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
 
         for(int i=0; i < numeros.length; i++){
             System.out.println(x * numeros[i]);
